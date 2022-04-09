2a6ad8c4f7e41c52f75007e87162d69089b23a11
Vladislav Shmyrev <vladislav.shmyrev@kuehne-nagel.com>
2022-04-09 11:09:37 +0300

    Refactor properties

--- src/main/java/com/giraffes/tgbot/GiraffeTelegramBot.java
+++ src/main/java/com/giraffes/tgbot/GiraffeTelegramBot.java
@@ -1,28 +1,16 @@
 package com.giraffes.tgbot;
 
 import com.giraffes.tgbot.property.BotProperties;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @Component
+@RequiredArgsConstructor
 public class GiraffeTelegramBot extends TelegramWebhookBot {
     private final BotProperties botProperties;
-
-    public GiraffeTelegramBot(BotProperties botProperties) {
-        super(createBotOptions(botProperties));
-        this.botProperties = botProperties;
-    }
-
-    private static DefaultBotOptions createBotOptions(BotProperties botProperties) {
-        DefaultBotOptions options = new DefaultBotOptions();
-//        options.setProxyHost(botProperties.getProxyHost());
-//        options.setProxyPort(botProperties.getProxyPort());
-//        options.setProxyType(botProperties.getProxyType());
-        return options;
-    }
 
     @Override
     public String getBotUsername() {
@@ -41,6 +29,6 @@ public class GiraffeTelegramBot extends TelegramWebhookBot {
 
     @Override
     public String getBotPath() {
-        return botProperties.getWebHookPath();
+        return null;
     }
 }
--- src/main/java/com/giraffes/tgbot/property/BotProperties.java
+++ src/main/java/com/giraffes/tgbot/property/BotProperties.java
@@ -4,18 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
 
 @Getter
 @Setter
 @Configuration
 @ConfigurationProperties("tg-bot")
 public class BotProperties {
-    private String webHookPath;
     private String botUserName;
     private String botToken;
-
-    private DefaultBotOptions.ProxyType proxyType;
-    private String proxyHost;
-    private int proxyPort;
 }
