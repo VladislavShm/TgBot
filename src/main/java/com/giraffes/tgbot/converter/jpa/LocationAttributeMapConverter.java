8ac15707eae70526d616c3f1f38b00dba59c7e84
Vladislav Shmyrev <vladislav.shmyrev@kuehne-nagel.com>
2022-04-24 10:46:47 +0300

    Location converter fix

--- src/main/java/com/giraffes/tgbot/converter/jpa/LocationAttributeMapConverter.java
+++ src/main/java/com/giraffes/tgbot/converter/jpa/LocationAttributeMapConverter.java
@@ -17,12 +17,20 @@ public class LocationAttributeMapConverter implements AttributeConverter<Map<Loc
     @Override
     @SneakyThrows
     public byte[] convertToDatabaseColumn(Map<LocationAttribute, String> customerInfo) {
+        if (customerInfo == null) {
+            return new byte[0];
+        }
+
         return new ObjectMapper().writeValueAsString(customerInfo).getBytes(StandardCharsets.UTF_8);
     }
 
     @Override
     @SneakyThrows
     public Map<LocationAttribute, String> convertToEntityAttribute(byte[] dbData) {
+        if (dbData == null || dbData.length == 0) {
+            return new HashMap<>();
+        }
+
         return ((Map<String, String>) new ObjectMapper().readValue(new String(dbData), HashMap.class))
                 .entrySet().stream().collect(Collectors.toMap(
                         entry -> Enum.valueOf(LocationAttribute.class, entry.getKey()),
